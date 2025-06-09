f04ac8d6ee269bcb09ff032601629cd757e7e3d0
MRLIGHT02 <saifali42020@gmail.com>
2025-06-09 14:51:18 +0530

    #

---
+++ SocialApp/Pages/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialApp.Pages
+{
+    public partial class HomePage : ContentPage
+    {
+
+
+        public HomePage()
+
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
