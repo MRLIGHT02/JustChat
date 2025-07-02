53335944d21902bb144a0bc718ff303504e2f9e3
MRLIGHT02 <saifali42020@gmail.com>
2025-07-02 16:09:01 +0530

    ##

---
+++ SocialApp/Controls/NoBorderEntry.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Handlers;
+
+using Microsoft.Maui.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialApp.Controls
+{
+    class NoBorderEntry : Entry
+    {
+
+        protected override void OnHandlerChanged()
+        {
+            base.OnHandlerChanged();
+
+            SetBackgroundChange();
+        }
+
+        private void SetBackgroundChange()
+        {
+#if ANDROID
+            if (Handler is EntryHandler entryHandler)
+            {
+
+                if (BackgroundColor == null)
+                {
+
+                    entryHandler.PlatformView.BackgroundTintList = ColorStateList.
+                }
+
+
+
+            }
+
+
+#endif
+
+        }
+    }
+}
+
--- SocialApp/Pages/OnBoardingPage.xaml.cs
+++ SocialApp/Pages/OnBoardingPage.xaml.cs
@@ -1,9 +1,18 @@
+using System.Threading.Tasks;
+
 namespace SocialApp.Pages;
 
 public partial class OnBoardingPage : ContentPage
 {
-	public OnBoardingPage()
-	{
-		InitializeComponent();
-	}
+    public OnBoardingPage()
+    {
+        InitializeComponent();
+    }
+
+
+
+    private async void goToLoginPage_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+    }
 }
